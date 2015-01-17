8882a6cf665c3d95f5e4b89dd257fdb3d5d66bec
miwara <aruim.jon@gmail.com>
2015-01-17 23:14:04 +0900

    add locale setup recipe

---
+++ site-cookbooks/locale/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# cookbook:: locale
+#
+template "/etc/locale.gen" do
+  source "/etc/locale.gen"
+
+  notifies :run, "execute[exec locale-gen]", :immediately
+end
+
+template "/etc/locale.conf" do
+  source "/etc/locale.conf"
+end
+
+execute "exec locale-gen" do
+  command "locale-gen"
+  action :nothing
+end
+
+
+

3a5f34307f5707aab349fdbdac88bbb9c076fa57
miwara <aruim.jon@gmail.com>
2015-01-18 19:40:17 +0900

    fcitxの各種設定ファイル

--- site-cookbooks/gui/recipes/fcitx.rb
+++ site-cookbooks/gui/recipes/fcitx.rb
@@ -16,3 +16,42 @@ otf-ipafont
     action :install
   end
 end
+
+template "/home/#{node['user']}/.config/fcitx/config" do
+  source "fcitx/config"
+
+  owner "#{node['user']}"
+  group "#{node['group']}"
+
+  notifies :run, "execute[reload fcitx]"
+end
+
+%w{
+fcitx-autoeng.config
+fcitx-chttrans.config
+fcitx-classic-ui.config
+fcitx-clipboard.config
+fcitx-imselector.config
+fcitx-keyboard.config
+fcitx-notify.config
+fcitx-pinyin-enhance.config
+fcitx-quickphrase.config
+fcitx-spell.config
+fcitx-unicode.config
+fcitx-xim.config
+fcitx-xkb.config
+}.each do |conffile|
+  template "/home/#{node['user']}/.config/fcitx/conf/#{conffile}" do
+    source "fcitx/conf/#{conffile}"
+
+    owner "#{node['user']}"
+    group "#{node['group']}"
+
+    notifies :run, "execute[reload fcitx]"
+  end
+end
+
+execute "reload fcitx" do
+  command "fcitx -r"
+  action :nothing
+end

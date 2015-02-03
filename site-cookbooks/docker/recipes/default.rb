4aa02822dcbf144c59414b95e01af5b367e7dd09
miwara <aruim.jon@gmail.com>
2015-02-03 23:30:25 +0900

    dockerを有効にしてすぐに使えるように

--- site-cookbooks/docker/recipes/default.rb
+++ site-cookbooks/docker/recipes/default.rb
@@ -5,3 +5,8 @@
 package "docker" do
   action :upgrade
 end
+
+execute "enable docker.service" do
+  command "systemctl enable docker.service"
+  not_if "systemctl status docker.service | grep enabled"
+end

b8033eafba6e9dc666e9f91d482671021d141ad2
miwara <aruim.jon@gmail.com>
2015-02-04 20:45:47 +0900

    dockerコマンドをroot以外でも使えるようにする

--- site-cookbooks/docker/recipes/default.rb
+++ site-cookbooks/docker/recipes/default.rb
@@ -10,3 +10,8 @@ execute "enable docker.service" do
   command "systemctl enable docker.service"
   not_if "systemctl status docker.service | grep enabled"
 end
+
+execute "subsume docker group" do
+  command "gpasswd -a #{node['user']} docker"
+  not_if "grep docker /etc/group | grep #{node['user']}"
+end

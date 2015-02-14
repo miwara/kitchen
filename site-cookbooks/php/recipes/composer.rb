e796a254b9ceb131a878484a8442e6cbcd13596c
miwara <aruim.jon@gmail.com>
2015-02-15 00:20:36 +0900

    インストールをマニュアル通りにやってみた

--- site-cookbooks/php/recipes/composer.rb
+++ site-cookbooks/php/recipes/composer.rb
@@ -1,8 +1,15 @@
 #
 # cookbook:: composer
 #
-remote_file "/usr/local/bin/composer" do
-  source "http://getcomposer.org/composer.phar"
-  mode "0755"
+execute "install composer" do
+  user node['user']
+  environment "HOME" => "/home/#{node['user']}"
+  cwd "/home/#{node['user']}"
+
+  command <<-EOH
+  curl -sS https://getcomposer.org/installer | php
+  sudo mv $HOME/composer.phar /usr/local/bin/composer
+  EOH
+
   not_if "test -e /usr/local/bin/composer"
 end
